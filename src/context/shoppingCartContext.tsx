f09d99dcba824d7b97e2cf03c16af75a81173953
Matthew Baillie <mbaillie93@gmail.com>
2022-10-13 23:44:48 -0700

    local storage added - completed

--- src/context/shoppingCartContext.tsx
+++ src/context/shoppingCartContext.tsx
@@ -1,5 +1,6 @@
 import { useContext, ReactNode, useState, createContext } from "react";
 import { ShoppingCart } from "../components/ShoppingCart";
+import { useLocalStorage } from "../hooks/useLocalStorage";
 type ShoppingCartProviderProps = {
   children: ReactNode;
 };
@@ -25,7 +26,10 @@ export function useShoppingCart() {
 
 export function ShoppingCartProvider({ children }: ShoppingCartProviderProps) {
   const [isOpen, setIsOpen] = useState(false);
-  const [cartItems, setCartItems] = useState<CartItem[]>([]);
+  const [cartItems, setCartItems] = useLocalStorage<CartItem[]>(
+    "shopping-cart",
+    []
+  );
   const cartQuantity = cartItems.reduce(
     (quantity, item) => item.quantity + quantity,
     0
